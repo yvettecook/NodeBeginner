89bd4f963ca6acf8201ea921af88027e5def6eed
Yvette <yvettecook@gmail.com>
2014-11-10 20:35:09 +0000

    Server code into module

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 var http = require('http');
 
-// Asyncronous
+function start() {
 
-// http.createServer(function(request, response) {
-// 	response.writeHead(200, { 'Content-Type': 'text/plain'} );
-// 	response.write('Hello world');
-// 	response.end();
-// }).listen(8888);
-
-// Syncronous
-
-function onRequest(request, response) {
+	function onRequest(request, response) {
 	console.log('Request received.');
 	response.writeHead(200, { 'Content-Type': 'text/plain'} );
 	response.write('Hello world');
 	response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log('Server has started.');
+
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start
 
-console.log('Server has started.');
