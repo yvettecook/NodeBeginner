3d346a6211099cfed2e9b6c5458c125a0fe3d73f
Yvette <yvettecook@gmail.com>
2014-11-10 19:27:32 +0000

    Syncronous vs. Asyncronous

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 var http = require('http');
 
-http.createServer(function(request, response) {
+// Asyncronous
+
+// http.createServer(function(request, response) {
+// 	response.writeHead(200, { 'Content-Type': 'text/plain'} );
+// 	response.write('Hello world');
+// 	response.end();
+// }).listen(8888);
+
+// Syncronous
+
+function onRequest(request, response) {
+	console.log('Request received.');
 	response.writeHead(200, { 'Content-Type': 'text/plain'} );
 	response.write('Hello world');
 	response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started.');
