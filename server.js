24595e6da176f50cad850f3253cd084e1f5e1d2f
Yvette <yvettecook@gmail.com>
2014-11-10 20:50:33 +0000

    server.js using the url module to extract to path being requested

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 var http = require('http');
+var url = require('url');
 
 function start() {
 
 	function onRequest(request, response) {
-	console.log('Request received.');
-	response.writeHead(200, { 'Content-Type': 'text/plain'} );
-	response.write('Hello world');
-	response.end();
-	}
 
-	http.createServer(onRequest).listen(8888);
-	console.log('Server has started.');
+		var pathname = url.parse(request.url).pathname;
+		console.log('Request for ' + pathname + ' received.');
+
+		response.writeHead(200, { 'Content-Type': 'text/plain'} );
+		response.write('Hello world');
+		response.end();
+		}
+
+		http.createServer(onRequest).listen(8888);
+		console.log('Server has started.');
 
 }
 
