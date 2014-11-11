cb7e62371929389ebee818bd249851afdc694fb2
Yvette <yvettecook@gmail.com>
2014-11-11 07:22:23 +0000

    Page 36 - Still the wrong way

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,14 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log('Request handler "start" was called.');
-	return 'Hello Start';
+	var content = "empty";
+
+	exec('find /', function(error, stdout, sterr) {
+		content = stdout
+	});
+
+	return content;
 }
 
 function upload() {
